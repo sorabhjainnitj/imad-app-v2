0b552aae82014283126f1e2782fdbbc679e2e7ff
sorabhjainnitj <sorabhjainnitj@gmail.com>
2017-02-03 09:43:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/articleone', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
