bc221c287338e208df505212aa06826db7cc3513
sorabhjainnitj <sorabhjainnitj@gmail.com>
2017-02-28 20:12:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@ console.log('Loaded!');
 var element1=document.getElementById('count');
 var element2=document.getElementById('counter');
 var cout=0;
-counter.onclick = function(){
+element.onclick = function(){
     cout=cout+1;
-    count.innerHtml=cout.toString();
+    element1.innerHtml=cout.toString();
     
     };
