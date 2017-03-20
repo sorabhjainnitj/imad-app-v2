d8cc692c7df99c776d4daaa78a38c342ad0899b6
sorabhjainnitj <sorabhjainnitj@gmail.com>
2017-03-20 22:56:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,8 @@ console.log('Loaded!');
 var button=document.getElementById('button');
 var area=document.getElementById('area');
 var cout=0;
-button.onclick = function(){
+button.click = function(){
     cout=cout+1;
     area.innerHTML=cout.toString();
     console.log('executing the task');
-    
     };
