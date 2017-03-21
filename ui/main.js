ee0dd16fbdf80bd9bed3a83f6868e44fd2d54489
sorabhjainnitj <sorabhjainnitj@gmail.com>
2017-03-21 10:37:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,9 @@
 console.log('Loaded!');
 var button=document.getElementById('button');
 var area=document.getElementById('area');
-var cout;
+var cout=1;
 button.onclick = function(){
-    var request=new XMLHttpRequest();
-    
-    request.onreadystatechange=function(){
-    if(request.readyState===XMLHttpRequest.DONE)
-    {   if(request.status==200)
-       { cout=request.responseText;
-        area.innerHTML=cout.toString();
-       }
-           
-       }
+   span.innerHTML=cout.toString();
 };
 
   request.open('GET',"https://sorabhjainnitj.imad.hasura-app.io/counter",true);
