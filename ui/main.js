798b03c4329ec562243bd9792d2b6adb3590dc0d
sorabhjainnitj <sorabhjainnitj@gmail.com>
2017-03-22 20:50:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@ console.log('Loaded!');
 var button=document.getElementById('button');
 var area=document.getElementById('area');
 
-var lgnbtn=document.getElementById('submitbtn');
-lgnbtn.onclick=function(){ 
+var createuserbtn=document.getElementById('submitbtn');
+createuserbtn.onclick=function(){ 
     var areaname=document.getElementById('userlog');
     var username=document.getElementById('name').value;
      var password=document.getElementById('userpassword').value;
@@ -18,6 +18,26 @@ lgnbtn.onclick=function(){
           }
     };
     request.open('POST','http://sorabhjainnitj.imad.hasura-app.io/createuser',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username, password: password }));
+    
+};
+var  lgnbtn=document.getElementById('login');
+lgnbtn.onclick=function(){ 
+    var areaname=document.getElementById('userlog');
+    var username=document.getElementById('name').value;
+     var password=document.getElementById('userpassword').value;
+     console.log(username);
+     console.log(password);
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+          { if(request.status===200)
+            { areaname.innerHTML='you have been registered successfully';
+            }
+          }
+    };
+    request.open('POST','http://sorabhjainnitj.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username: username, password: password }));
     
